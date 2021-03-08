49d6fdb5e450764e2dba4319b69bb1a55f8b93a9
Sukhwinder Singh <sukhwinder.singh@zextras.com>
2021-03-08 15:00:04 +0100

    Add sass package and router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { route } from './routes'
 import Homepage from './pages/Homepage'
+import SettingsPage from './pages/Settings'
 
 export function App() {
-  return <Homepage />
+  return (
+    <BrowserRouter>
+      <Switch>
+        {[
+          { path: route.home, component: Homepage },
+          { path: route.settings, component: SettingsPage }
+        ].map(({ path, component }, i) => (
+          <Route exact key={i} path={path} component={component} />
+        ))}
+      </Switch>
+    </BrowserRouter>
+  )
 }
--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,3 +1,22 @@
+import { Navbar } from 'trunx'
+
 export function Topbar() {
-  return null
+  return (
+    <Navbar isLight>
+      <Navbar.Brand>
+        <Navbar.Item>
+          <img
+            src='https://bulma.io/images/bulma-logo.png'
+            width='112'
+            height='28'
+          />
+        </Navbar.Item>
+      </Navbar.Brand>
+      <Navbar.Menu>
+        <Navbar.Start>
+          <Navbar.Item>Settings</Navbar.Item>
+        </Navbar.Start>
+      </Navbar.Menu>
+    </Navbar>
+  )
 }
